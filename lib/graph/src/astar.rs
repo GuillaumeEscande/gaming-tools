c3a1896237d51945a2870bb4fcdd7698cab2c0d1
Guillaume Escande <escande.guillaume@gmail.com>
2021-04-30 21:36:27 +0200

    add default model

--- lib/graph/src/astar.rs
+++ lib/graph/src/astar.rs
@@ -1,14 +1,59 @@
+// Graph module with implementation Graph search algorithms
 pub mod graph {
+    use std::collections::LinkedList;
+    use crate::model;
 
-    pub fn resolve_astar(){
-        
+    // Research shortesd path on the graph between start and target
+
+    pub fn resolve_astar(
+        graph: Box<dyn model::graph::Graph>,
+        start: Box<dyn model::graph::Node>,
+        target: Box<dyn model::graph::Node> ) -> LinkedList<Box<dyn model::graph::Node>> {
+
+        let best_way = LinkedList::new();
+        return best_way;
     }
 
 }
+
+
 #[cfg(test)]
 mod tests {
+    use std::collections::LinkedList;
+    use crate::model;
+    use crate::model::graph::default;
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_nominal() {
+
+        let mut nodes : LinkedList< Box<dyn model::graph::Node> > = LinkedList::new();
+        let mut links : LinkedList< Box<dyn model::graph::Link> > = LinkedList::new();
+
+        let node1 = Box::<default::DefaultNode>::new(default::DefaultNode{
+            name: String::from("test"), nexts: LinkedList::new()});
+        let link1 = Box::<default::DefaultLink>::new(default::DefaultLink{
+            cost: 2.5});
+        let node2 = Box::<default::DefaultNode>::new(default::DefaultNode{
+            name: String::from("test2"), nexts: LinkedList::new()});
+
+        node1.nexts.push_back((link1, node2));
+
+        nodes.push_back(node1);
+        nodes.push_back(node1);
+
+        links.push_back(link1);
+
+        let grah = Box::<default::DefaultGraph>::new(default::DefaultGraph{
+            nodes: nodes,
+            links: links});
+
+
+        let result : LinkedList::<Box::<dyn model::graph::Node>> = graph::resolve_astar(
+            grah,
+            node1,
+            node2 );
+
+        assert!(result.len() == 0);
     }
 }
--- lib/graph/src/model.rs
+++ lib/graph/src/model.rs
@@ -1,24 +1,63 @@
 pub mod graph {
-    trait Node {
-        fn name(&self)->str;
+    use std::collections::LinkedList;   
+
+    pub trait Node {
+        fn name(self) -> String;
+        fn next(self) -> LinkedList< (Box<dyn Link>, Box<dyn Node>) >;
     }
     
-    trait Link {
-        fn quack(&self);
+    pub trait Link {
+        fn cost(self) -> f64;
     }
     
-    trait Graph {
-        fn quack(&self);
+    pub trait Graph {
+        fn nodes(self) -> LinkedList< Box<dyn Node> >;
+        fn links(self) -> LinkedList< Box<dyn Link> >;
     }
  
     
+    pub mod default {
+        use std::collections::LinkedList; 
+        use crate::model; 
+
+        pub struct DefaultLink {
+            pub cost: f64
+        }
     
-    #[cfg(test)]
-    mod tests {
+        impl model::graph::Link for DefaultLink {
+            fn cost(self) -> f64{
+                return self.cost;
+            }
+        }
     
-        #[test]
-        fn test_output() {
-            assert_eq!(3, 3);
+        pub struct DefaultNode {
+            pub name: String,
+            pub nexts: LinkedList::< (Box<dyn model::graph::Link>, Box<dyn model::graph::Node>) >
+        }
+    
+        impl model::graph::Node for DefaultNode {
+            fn name(self) -> String{
+                return self.name;
+            }
+            fn next(self) -> LinkedList< (Box<dyn model::graph::Link>, Box<dyn model::graph::Node>) >{
+                return self.nexts;
+            }
+        }
+
+        pub struct DefaultGraph {
+            pub nodes: LinkedList< Box<dyn model::graph::Node> >,
+            pub links: LinkedList< Box<dyn model::graph::Link> >
+        }
+
+        impl model::graph::Graph for DefaultGraph {
+
+            fn nodes(self) -> LinkedList< Box<dyn model::graph::Node> >{
+                return self.nodes;
+            }
+
+            fn links(self) -> LinkedList< Box<dyn model::graph::Link> >{
+                return self.links;
+            }
         }
     }
 }
