1418d59db9e20d327ed81a9ec5f030881a6c61e6
Guillaume Escande <escande.guillaume@gmail.com>
2021-05-06 12:52:07 +0200

    Add board 2D

--- lib/base/src/board.rs
+++ lib/base/src/board.rs
@@ -4,17 +4,38 @@ pub mod base {
     pub trait BoardCase< T: Eq + PartialEq + Sized > {
         fn position(&self)->Vec<i64>;
         fn get_value(&self)->T;
-        fn goto(&self, origin: &T, target: &T)->i64;
+        fn distance(&self, target: &T)->i64;
+        fn get_board(&self)->&dyn Board<T>;
     }
 
     pub trait Board< T : Eq + PartialEq + Sized > {
         fn neighbors(&self, origin: &dyn BoardCase<T> )->Vec<&dyn BoardCase<T> >;
-        fn sub_board(&self, origin: Vec<i64>, size: Vec<i64> )->dyn Board<T>;
+        fn sub_board(&self, origin: &dyn BoardCase<T>, size: Vec<i64> )->dyn Board<T>;
+        fn distance(&self, origin: &dyn BoardCase<T>, target: &dyn BoardCase<T>)->i64;
+        fn get_dim(&self) -> Vec<i64>;
         fn print(&self);
     }
 
     pub struct Board2D< T : Eq + PartialEq + Sized > {
         board : Vec< Vec<T> >
+    }
+
+    impl< T : Eq + PartialEq + Sized > Board<T> for Board2D<T>{
+        fn neighbors(&self, origin: &dyn BoardCase<T> )->Vec<&dyn BoardCase<T> >{
+
+        }
+        fn sub_board(&self, origin: &dyn BoardCase<T>, size: Vec<i64> )->dyn Board<T>{
+
+        }
+        fn distance(&self, origin: &dyn BoardCase<T>, target: &dyn BoardCase<T>)->i64{
+
+        }
+        fn get_dim(&self) -> Vec<i64>{
+
+        }
+        fn print(&self){
+            
+        }
     }
 
 
