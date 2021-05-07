7a4a25852517a458bbc1016fe864b4f129aec186
Guillaume Escande <escande.guillaume@gmail.com>
2021-05-07 13:46:54 +0200

    set corrdinate -x +x + game

--- lib/base/src/board.rs
+++ lib/base/src/board.rs
@@ -2,15 +2,14 @@ pub mod base {
     use std::vec::Vec;
 
     pub trait BoardCase< T: Eq + PartialEq + Sized > {
-        fn position(&self)->Vec<usize>;
+        fn position(&self)->Vec<i64>;
         fn get_value(&self)->&T;
     }
 
     pub trait Board< T : Eq + PartialEq + Sized > {
         fn neighbors(&self, origin: &dyn BoardCase<T> )->Vec<&dyn BoardCase<T> >;
-        //fn sub_board(&self, origin: &dyn BoardCase<T>, size: Vec<i64> )->dyn Board<T>;
         fn distance(&self, origin: &dyn BoardCase<T>, target: &dyn BoardCase<T>)->i64;
-        fn get(&self, pos: &Vec<usize>)->&dyn BoardCase<T>;
+        fn get(&self, pos: &Vec<i64>)->&dyn BoardCase<T>;
         fn size(&self) -> Vec<usize>;
         fn print(&self);
     }
@@ -19,14 +18,14 @@ pub mod base {
         use crate::board::base;
 
         pub struct HexagonCase< T : Eq + PartialEq + Sized > {
-            line1 : usize,
-            line2 : usize,
-            line3 : usize,
+            line1 : i64,
+            line2 : i64,
+            line3 : i64,
             value: T,
         }
 
         impl< T : Eq + PartialEq + Sized > base::BoardCase< T > for HexagonCase< T > {
-            fn position(&self)->Vec<usize>{
+            fn position(&self)->Vec<i64>{
                 return vec!(self.line1, self.line2, self.line3);
             }
             fn get_value(&self)->&T{
@@ -43,16 +42,17 @@ pub mod base {
             pub fn new(size : usize, default: &T) -> Hexagon<T>{
 
                 let mut board = Vec::with_capacity(size);
+                let real_size = 1 + size * 2;
 
-                for c1 in 0..size{
-                    board.push(Vec::with_capacity(size));
-                    for c2 in 0..size{
-                        board[c1].push(Vec::with_capacity(size));
-                        for c3 in 0..size{
+                for c1 in 0..real_size{
+                    board.push(Vec::with_capacity(real_size));
+                    for c2 in 0..real_size{
+                        board[c1].push(Vec::with_capacity(real_size));
+                        for c3 in 0..real_size{
                             let case = HexagonCase::<T>{
-                                line1 : c1,
-                                line2 : c2,
-                                line3 : c3,
+                                line1 : c1 as i64 - size as i64,
+                                line2 : c2 as i64 - size as i64,
+                                line3 : c3 as i64 - size as i64,
                                 value: default.clone(),
                             };
                             board[c1][c2].push(case);
@@ -69,28 +69,30 @@ pub mod base {
 
         impl< T : Eq + PartialEq + Sized > base::Board< T > for Hexagon< T > {
             fn neighbors(&self, origin: &dyn base::BoardCase<T> )->Vec<&dyn base::BoardCase<T> >{
-                let pos1 : usize = origin.position()[0] as usize;
-                let pos2 : usize = origin.position()[1] as usize;
-                let pos3 : usize = origin.position()[2] as usize;
+                let pos1 : usize = (origin.position()[0] + self.size as i64) as usize;
+                let pos2 : usize = (origin.position()[1] + self.size as i64) as usize;
+                let pos3 : usize = (origin.position()[2] + self.size as i64) as usize;
+
+                let real_size = 1 + self.size * 2;
 
                 let mut neighbors : Vec<&dyn base::BoardCase<T>> = Vec::with_capacity(6);
 
                 if pos1 > 1 {
                     neighbors.push(&self.board[pos1-1][pos2][pos3]);
                 }
-                if pos1 < self.size - 2 {
+                if pos1 < real_size - 2 {
                     neighbors.push(&self.board[pos1+1][pos2][pos3]);
                 }
                 if pos2 > 1 {
                     neighbors.push(&self.board[pos1][pos2-1][pos3]);
                 }
-                if pos2 < self.size -2 {
+                if pos2 < real_size -2 {
                     neighbors.push(&self.board[pos1][pos2+1][pos3]);
                 }
                 if pos3 > 1 {
                     neighbors.push(&self.board[pos1][pos2][pos3-1]);
                 }
-                if pos3 < self.size -2 {
+                if pos3 < real_size -2 {
                     neighbors.push(&self.board[pos1][pos2][pos3+1]);
                 }
                 return neighbors;
@@ -107,8 +109,12 @@ pub mod base {
                 return vec!(self.size, self.size, self.size);
             }
 
-            fn get(&self, pos: &Vec<usize>)->&dyn base::BoardCase<T>{
-                &self.board[pos[0]][pos[1]][pos[2]]
+            fn get(&self, pos: &Vec<i64>)->&dyn base::BoardCase<T>{
+                let pos1 : usize = (pos[0] + self.size as i64) as usize;
+                let pos2 : usize = (pos[1] + self.size as i64) as usize;
+                let pos3 : usize = (pos[2] + self.size as i64) as usize;
+
+                &self.board[pos1][pos2][pos3]
             }
             fn print(&self){
                 //TODO
@@ -128,13 +134,16 @@ mod tests {
     fn test_nominal() {
         let board = Hexagon::<i64>::new( 7, &0);
         assert_eq!(board.size(), vec!(7, 7, 7));
-        assert_eq!(board.neighbors( board.get( &vec!(3 as usize, 3 as usize, 3 as usize) ) ).len(), 6);
-        assert_eq!(board.neighbors( board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ) ).len(), 3);
+        assert_eq!(board.neighbors( board.get( &vec!(3, 3, 0) ) ).len(), 3);
+        assert_eq!(board.neighbors( board.get( &vec!(0, -3, 3) ) ).len(), 3);
+        assert_eq!(board.neighbors( board.get( &vec!(-3, 2, 1) ) ).len(), 3);
+        assert_eq!(board.neighbors( board.get( &vec!(-1, -1, 2) ) ).len(), 6);
+        assert_eq!(board.neighbors( board.get( &vec!(0, 0, 0) ) ).len(), 6);
 
-        assert_eq!(board.distance( board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ), board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ) ), 0);
-        assert_eq!(board.distance( board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ), board.get( &vec!(1 as usize, 0 as usize, 0 as usize) ) ), 1);
-        assert_eq!(board.distance( board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ), board.get( &vec!(0 as usize, 3 as usize, 0 as usize) ) ), 3);
-        assert_eq!(board.distance( board.get( &vec!(0 as usize, 0 as usize, 0 as usize) ), board.get( &vec!(1 as usize, 2 as usize, 1 as usize) ) ), 2);
+        assert_eq!(board.distance( board.get( &vec!(0, 0, 0) ), board.get( &vec!(0, 0, 0) ) ), 0);
+        assert_eq!(board.distance( board.get( &vec!(0, 0, 0) ), board.get( &vec!(1, 0, 0) ) ), 1);
+        assert_eq!(board.distance( board.get( &vec!(0, 0, 0) ), board.get( &vec!(0, 3, 0) ) ), 3);
+        assert_eq!(board.distance( board.get( &vec!(0, 0, 0) ), board.get( &vec!(1, 2, 1) ) ), 2);
 
     }
 }   
--- lib/base/src/game.rs
+++ lib/base/src/game.rs
@@ -1,12 +1,21 @@
 pub mod base {
     use std::collections::LinkedList;
+    use crate::board::base;
 
+    pub trait State : Eq + PartialEq + Sized + Clone {
+
+    }
     pub trait Action : Eq + PartialEq + Sized + Clone {
 
     }
-    pub trait Game : <B: Board, A: Action> {
+    pub trait Player: Eq + PartialEq + Sized + Clone {
+    }
+    pub trait Game <T : Eq + PartialEq + Sized + Clone, P: Player, B: base::Board<T>, A: Action> {
         fn state(&self) -> &B;
         fn actions(&self) -> LinkedList< A >;
+        fn score(&self, player : &P) -> i64;
+        fn is_terminate(&self) -> bool;
+        fn apply(&self, action: &A) -> Self;
     }
 
 }
